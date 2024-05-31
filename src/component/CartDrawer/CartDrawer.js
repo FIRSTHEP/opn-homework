1cb68c951881840a7d98f0e385f8149d61f6913e
Adithep Sudcharee <adithep.s@rznet.com>
2024-05-31 08:15:48 +0700

    update

--- src/component/CartDrawer/CartDrawer.js
+++ src/component/CartDrawer/CartDrawer.js
@@ -2,14 +2,6 @@ import React from 'react';
 import { Drawer, Row, Col, Button, Divider, Image } from 'antd';
 import { CloseOutlined, MinusOutlined, PlusOutlined } from '@ant-design/icons';
 
-const buttonStyle = {
-  backgroundColor: '#322f3c',
-  border: 0,
-  color: 'white',
-  width: '100%',
-  height: '55px',
-};
-
 const CartDrawer = ({ isOpen, onClose, cartItems, decreaseQty, increaseQty, getTotalCardAmount, getTotalPrice, clearCart }) => {
   return (
     <Drawer
@@ -26,7 +18,7 @@ const CartDrawer = ({ isOpen, onClose, cartItems, decreaseQty, increaseQty, getT
               type="text"
               size="large"
               icon={<CloseOutlined style={{ color: 'white', fontSize: '16px' }} />}
-              style={{ ...buttonStyle, height: '50px', width: '50px', float: 'right' }}
+              style={{ backgroundColor: '#EA7C69', boxShadow: '0 0 10px rgba(234, 124, 105, 0.5)', float: 'right', height: '50px', width: '50px' }}
               onClick={onClose}
             />
           </Col>
@@ -64,13 +56,13 @@ const CartDrawer = ({ isOpen, onClose, cartItems, decreaseQty, increaseQty, getT
               </Row>
               <Row justify="end" gutter={[16, 16]}>
                 <Col xs={5} sm={5} md={5} lg={5} xl={5}>
-                  <Button icon={<MinusOutlined style={{ fontSize: '14px' }} />} size="large" onClick={() => decreaseQty(index)} style={buttonStyle} />
+                  <Button icon={<MinusOutlined style={{ fontSize: '14px' }} />} size="large" onClick={() => decreaseQty(index)} style={{ width: '100%', height: 55, backgroundColor: '#322f3c', border: 0, color: "white" }} />
                 </Col>
-                <Col xs={14} sm={14} md={14} lg={14} xl={14} style={{ textAlign: 'center', ...buttonStyle, fontSize: '1.2em', fontWeight: 700, display: 'flex', alignItems: 'center', justifyContent: 'center', borderRadius: '6px' }}>
+                <Col xs={14} sm={14} md={14} lg={14} xl={14} style={{ textAlign: 'center', backgroundColor: '#322f3c', color: 'white', fontSize: '1.2em', fontWeight: 700, display: 'flex', alignItems: 'center', justifyContent: 'center', borderRadius: '6px' }}>
                   <span>{item.qty}</span>
                 </Col>
                 <Col xs={5} sm={5} md={5} lg={5} xl={5}>
-                  <Button icon={<PlusOutlined style={{ fontSize: '14px' }} />} size="large" onClick={() => increaseQty(index)} style={buttonStyle} />
+                  <Button icon={<PlusOutlined style={{ fontSize: '14px' }} />} size="large" onClick={() => increaseQty(index)} style={{ width: '100%', height: 55, backgroundColor: '#322f3c', border: 0, color: "white" }} />
                 </Col>
               </Row>
               <br />
@@ -89,19 +81,12 @@ const CartDrawer = ({ isOpen, onClose, cartItems, decreaseQty, increaseQty, getT
         </Row>
         <Row justify="end" style={{ paddingTop: '20px', paddingBottom: '30px' }}>
           <Col span={24}>
-            <Button
-              type="primary"
-              size="large"
-              block
-              style={{
-                ...buttonStyle,
-                backgroundColor: '#EA7C69',
-                boxShadow: '0 0 10px rgba(234, 124, 105, 0.5)',
-                marginBottom: '1em',
-              }}
-            >
-              Continue to Payment
-            </Button>
+            <Button type="primary" size="large" block style={{
+              backgroundColor: '#EA7C69',
+              boxShadow: '0 0 10px rgba(234, 124, 105, 0.5)',
+              color: 'white',
+              marginBottom: '1em'
+            }}>Continue to Payment</Button>
           </Col>
         </Row>
       </div>
